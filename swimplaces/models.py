8fc7c2e7b4b94d67e96d5024eac37b93b3ac378d
jiri.one <nemec@jiri.one>
2022-08-12 15:21:16 +0200

    successful import of csv

--- swimplaces/models.py
+++ swimplaces/models.py
@@ -26,31 +26,35 @@ class SwimPlace(models.Model):
     longitude = models.FloatField()
     latitude = models.FloatField()
     name = models.CharField(max_length=100)
-    category = models.ForeignKey("Category", on_delete=models.PROTECT)
-    rating = models.FloatField()
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, null=True, blank=True)
+    rating = models.FloatField(null=True, blank=True)
     image_url = models.URLField()
-    import_id = models.CharField(max_length=20)
-    description1 = models.CharField(max_length=200)
-    description2 = models.CharField(max_length=200)
-    address = models.CharField(max_length=200, 
-                               default=None)
-    web = models.URLField()
-    e_mail = models.EmailField()
-    phone_number = models.CharField(max_length=15)
+    import_id = models.CharField(max_length=20, null=True, blank=True)
+    description1 = models.CharField(max_length=200, null=True, blank=True)
+    description2 = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    web = models.URLField(null=True, blank=True)
+    e_mail = models.EmailField(null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
     refreshment = models.CharField(choices=REFRESH,      
-                                   max_length=50)
+                                   max_length=50, null=True, blank=True)
     diving = models.CharField(choices=DIVING,
-                              max_length=50)
+                              max_length=50, null=True, blank=True)
     entrance = models.CharField(choices=ENTRANCE,
-                                max_length=20)
-    parking = models.CharField(max_length=50)
-    link = models.URLField()
+                                max_length=20, null=True, blank=True)
+    parking = models.CharField(max_length=50, null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
     nudist_beach = models.CharField(choices=NUDE,
-                                    max_length=30)
-    video = models.URLField()
+                                    max_length=30, null=True, blank=True)
+    video = models.URLField(null=True, blank=True)
     dog_swimming = models.CharField(choices=DOGS,
-                                    max_length=30)
+                                    max_length=30, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    def __str__(self):
+        return self.name
--- utils/from_csv_to_django.py
+++ utils/from_csv_to_django.py
@@ -27,7 +27,7 @@ link
 nudist_beach
 video
 dog_swimming
-"""
+""".strip().split('\n')
 
 @contextmanager
 def django_context():
@@ -42,14 +42,37 @@ def django_context():
         sys_path.remove(django_dir)
         del application
 
-with django_context():
-    from swimplaces.models import SwimPlace, Category
-    fieldnames = FIELDNAMES.strip().split('\n')
-    categories_dictcreate_categories()
+
+def create_categories(Category):
     with open("swimplaces_export.csv", "r") as file:
-        reader = csv.DictReader(file, fieldnames=fieldnames, delimiter=';')
+        reader = csv.DictReader(file, fieldnames=FIELDNAMES, delimiter=';')
         next(reader, None) # skip first line
         for row in reader:
-            dict_row = create_(line.decode())
-            # record = SwimPlace(**dict_row)
-            # record.save()
+            if row["category"]: # prevent empty category
+                try:
+                    category = Category.objects.get(name=row["category"])
+                except Category.DoesNotExist:
+                    category = Category(name=row["category"])
+                    category.save()
+
+def create_swimplaces(SwimPlace, Category):
+    with open("swimplaces_export.csv", "r") as file:
+        reader = csv.DictReader(file, fieldnames=FIELDNAMES, delimiter=';')
+        next(reader, None) # skip first line
+        for row in reader:
+            for key, value in dict(row).items(): # prevent empty string from csv
+                if not value:
+                    row[key] = None
+            if row["category"]:
+                row["category"] = Category.objects.get(name=row["category"])
+            swimplace = SwimPlace(**row)
+            swimplace.save()
+
+with django_context():
+    from swimplaces.models import SwimPlace, Category
+    # clean all previous imports
+    SwimPlace.objects.all().delete()
+    Category.objects.all().delete()
+    # and then import all
+    create_categories(Category)
+    create_swimplaces(SwimPlace, Category)
